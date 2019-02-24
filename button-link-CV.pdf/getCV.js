6dad2570e718d6632b302d8559e4cb7f1eb7c78f
viy87 <V.Yokhym@gmail.com>
2019-02-24 16:33:06 +0300

    add project to gitHub

---
+++ button-link-CV.pdf/getCV.js
@@ -0,0 +1,8 @@
+﻿var nameUser = prompt('Enter your name');
+
+while (nameUser == null || nameUser == "") {
+	alert('You did not enter your name');
+	var nameUser = prompt('Please, enter your name');
+}
+
+alert('Hello ' + nameUser + '! ' + 'Now you can get CV.');
